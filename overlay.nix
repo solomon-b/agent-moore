58cd5796d7d507675b853c61e3bd9bec2aac899d
solomon <ssbothwell@gmail.com>
2023-12-06 12:03:14 -0800

    Fix nix build

--- overlay.nix
+++ overlay.nix
@@ -3,6 +3,8 @@ final: prev: {
     overrides = prev.lib.composeExtensions (old.overrides or (_: _: { }))
       (hfinal: hprev: {
         agent-moore = hfinal.callCabal2nix "agent-moore" (./.) { };
+        bifunctors = hfinal.bifunctors_5_6_1;
+        semigroupoids = hfinal.semigroupoids_6_0_0_1;
       });
   });
 }
