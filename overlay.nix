bf9b4c6b9cbbb1e00c5f779a6583459078ed37ef
solomon <ssbothwell@gmail.com>
2023-12-06 12:08:49 -0800

    Move bifunctors and semigroupoid overrides into overrideScope

--- overlay.nix
+++ overlay.nix
@@ -2,9 +2,10 @@ final: prev: {
   haskellPackages = prev.haskellPackages.override (old: {
     overrides = prev.lib.composeExtensions (old.overrides or (_: _: { }))
       (hfinal: hprev: {
-        agent-moore = hfinal.callCabal2nix "agent-moore" (./.) { };
-        bifunctors = hfinal.bifunctors_5_6_1;
-        semigroupoids = hfinal.semigroupoids_6_0_0_1;
+        agent-moore = (hfinal.callCabal2nix "agent-moore" (./.) { }).overrideScope (hfinal': hprev': {
+          bifunctors = hfinal.bifunctors_5_6_1;
+          semigroupoids = hfinal.semigroupoids_6_0_0_1;
+        });
       });
   });
 }
