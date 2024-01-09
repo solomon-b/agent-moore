2715250cebc30e60faf8a0f58ddcc01609cf0871
solomon <ssbothwell@gmail.com>
2024-01-09 08:56:54 -0800

    Adds MooreM module

--- src/Machines.hs
+++ src/Machines.hs
@@ -20,6 +20,19 @@ newtype Fix f = Fix {unFix :: f (Fix f)}
 -- with a By^A Moore Machine. Its the universal thing that can be put
 -- together with a By^A Moore Machine. They're not just two different
 -- definitions, they are dual in certain sense." -- David Spivak
+
+-- annihilate :: Monad m => MooreM' m o i -> MealyM' m i o -> Fix m
+-- annihilate (MooreM' server) b@(MealyM' botBehavior) = Fix $ do
+--   (i, nextServer) <- server
+--   xs <- fromListT $ botBehavior i
+--   let o = fmap fst $ xs
+--       server' = nextServer o
+--   pure $
+--     annihilate server' $ case xs of
+--       [] -> b
+--       _ -> snd $ last xs
+
+    
 -- annihilate :: Moore' i o -> Mealy' o i -> Fix Identity
 -- annihilate (Moore' moore) (MealyM' mealy) =
 --   Fix $ do
