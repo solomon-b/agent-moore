ca4875b27a5673590176cf911ab75e8f28ad08eb
solomon <ssbothwell@gmail.com>
2024-02-09 22:54:34 -0800

    Remove overlay.nix

--- flake.nix
+++ flake.nix
@@ -4,33 +4,41 @@
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
     flake-utils.url = github:numtide/flake-utils;
-    monoidal-functors.url = github:solomon-b/monoidal-functors;
+    monoidal-functors = {
+      url = github:solomon-b/monoidal-functors/solomon/removes-overlay;
+      flake = false;
+    };
   };
 
   outputs = { self, nixpkgs, flake-utils, monoidal-functors }:
     let
-      overlay = import ./overlay.nix;
-      overlays = [ overlay monoidal-functors.overlays.default ];
+      ghcVersion = "963";
+      compiler = "ghc${ghcVersion}";
     in
     flake-utils.lib.eachSystem [ "x86_64-linux" ]
       (system:
-        let pkgs = import nixpkgs { inherit system overlays; };
+        let pkgs = import nixpkgs { inherit system; };
+            hsPkgs = pkgs.haskell.packages.${compiler}.override {
+              overrides = hfinal: hprev: {
+                agent-moore = (hfinal.callCabal2nix "agent-moore" ./. { });
+                bifunctors = hfinal.bifunctors_5_6_1;
+                monoidal-functors = hfinal.callCabal2nix "monoidal-functors" monoidal-functors {};
+                semigroupoids = hfinal.semigroupoids_6_0_0_1;
+              };
+            };
         in rec {
-          devShells.default = pkgs.haskellPackages.shellFor {
+          devShells.default = hsPkgs.shellFor {
             packages = p: [ p.agent-moore ];
             buildInputs = [
-              pkgs.haskellPackages.cabal-install
-              pkgs.haskellPackages.ghc
-              pkgs.haskellPackages.haskell-language-server
+              pkgs.cabal-install
+              pkgs.haskell.compiler.${compiler}
+              pkgs.haskell.packages.${compiler}.haskell-language-server
               pkgs.nixfmt
 
               pkgs.pkg-config
             ];
           };
           formatter = pkgs.nixpkgs-fmt;
-          packages.default = pkgs.haskellPackages.agent-moore;
-          packages.co-prompt = pkgs.haskellPackages.agent-moore;
-        }) // {
-      overlays.default = overlay;
-    };
+          packages.default = hsPkgs.agent-moore;
+        });
 }
